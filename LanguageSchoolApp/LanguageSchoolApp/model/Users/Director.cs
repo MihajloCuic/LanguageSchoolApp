a48050722b7c9ac54dca1f642f73ad2c3d0a9c34
MihajloCuic <158320399+MihajloCuic@users.noreply.github.com>
2025-02-18 19:03:58 +0100

    Created model for 3 types of users

---
+++ LanguageSchoolApp/LanguageSchoolApp/model/Users/Director.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguageSchoolApp.model.Users
+{
+    public class Director : User
+    {
+        public static readonly string filename = Path.Combine("..", "..", "..", "data", "Director.json");
+
+        public Director() { }
+        public Director(string _name, string _surname, Gender _gender, DateTime _birthday, int _phoneNumber, string _email, string _password)
+        {
+            Name = _name;
+            Surname = _surname;
+            Gender = _gender;
+            Birthday = _birthday;
+            PhoneNumber = _phoneNumber;
+            Email = _email;
+            Password = _password;
+        }
+    }
+}
---
+++ LanguageSchoolApp/LanguageSchoolApp/model/Users/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace LanguageSchoolApp.model.Users
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public Gender Gender { get; set; }
+        public DateTime Birthday { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public User() { }
+
+        public User(string _name, string _surname, Gender _gender, DateTime _birthday, int _phoneNumber, string _email, string _password)
+        {
+            Name = _name;
+            Surname = _surname;
+            Gender = _gender;
+            Birthday = _birthday;
+            PhoneNumber = _phoneNumber;
+            Email = _email;
+            Password = _password;
+        }
+    }
+}
