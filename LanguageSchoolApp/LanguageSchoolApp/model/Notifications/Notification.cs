660b33091c4aa8dc745520a287e350f3a133b93d
MihajloCuic <158320399+MihajloCuic@users.noreply.github.com>
2025-02-18 19:06:02 +0100

    Created model for Notification

---
+++ LanguageSchoolApp/LanguageSchoolApp/model/Notifications/Notification.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LanguageSchoolApp.model.Users;
+
+namespace LanguageSchoolApp.model.Notifications
+{
+    public class Notification
+    {
+        public static readonly string filename = Path.Combine("..", "..", "..", "data", "Notifications.json");
+
+        public User Sender { get; set; }
+        public User Receiver { get; set; }
+        public NotificationType NotificationType { get; set; }
+        public string Message { get; set; }
+
+        public Notification() { }
+
+        public Notification(User _sender, User _receiver, NotificationType _notificationType, string _message)
+        {
+            Sender = _sender;
+            Receiver = _receiver;
+            NotificationType = _notificationType;
+            Message = _message;
+        }
+    }
+}
