c2c0a103b368786fea50db6014b19792d7ecdd70
MihajloCuic <158320399+MihajloCuic@users.noreply.github.com>
2025-02-18 19:04:40 +0100

    Created model for Course and CourseApplication

---
+++ LanguageSchoolApp/LanguageSchoolApp/model/Courses/CourseApplication.cs
@@ -0,0 +1,28 @@
+﻿using LanguageSchoolApp.model.Users;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguageSchoolApp.model.Courses
+{
+    public class CourseApplication
+    {
+        public Student Student { get; set; }
+        public Course Course { get; set; }
+        public bool Paused { get; set; }
+        public bool Accepted { get; set; }
+
+        public CourseApplication() { }
+
+        public CourseApplication(Student _student, Course _course, bool _paused, bool _accepted) { 
+            Student = _student;
+            Course = _course;
+            Paused = _paused;
+            Accepted = _accepted;
+        }
+
+    }
+}
---
+++ LanguageSchoolApp/LanguageSchoolApp/model/Courses/LanguageProficiency.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguageSchoolApp.model.Courses
+{
+    public class LanguageProficiency
+    {
+        public string LanguageName { get; set; }
+        public LanguageLevel LanguageLevel { get; set; }
+
+        public LanguageProficiency() { }
+
+        public LanguageProficiency(string _languageName, LanguageLevel _languageLevel) { 
+            LanguageName = _languageName;
+            LanguageLevel = _languageLevel;
+        }
+    }
+}
