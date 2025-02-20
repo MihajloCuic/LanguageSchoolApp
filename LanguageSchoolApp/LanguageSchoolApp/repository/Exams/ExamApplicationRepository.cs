4bcddcd4a5e0adcf1cf738a95410579b7b2c48e2
MihajloCuic <158320399+MihajloCuic@users.noreply.github.com>
2025-02-20 13:56:28 +0100

    Added exam related repositories

---
+++ LanguageSchoolApp/LanguageSchoolApp/repository/Exams/ExamApplicationRepository.cs
@@ -0,0 +1,44 @@
+﻿using LanguageSchoolApp.model.Exams;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguageSchoolApp.repository.Exams
+{
+    public class ExamApplicationRepository
+    {
+        public static readonly string filename = Path.Combine("..", "..", "..", "data", "ExamApplications.json");
+
+        public static void WriteToFile(Dictionary<int, ExamApplication> allExamApplications)
+        {
+            try
+            {
+                string serializedExamApplications = JsonConvert.SerializeObject(allExamApplications, Formatting.Indented);
+                File.WriteAllText(filename, serializedExamApplications);
+            }
+            catch (IOException e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        public static Dictionary<int, ExamApplication> ReadFromFile()
+        {
+            Dictionary<int, ExamApplication> allExamApplications = new Dictionary<int, ExamApplication>();
+            try
+            {
+                string data = File.ReadAllText(filename);
+                allExamApplications = JsonConvert.DeserializeObject<Dictionary<int, ExamApplication>>(data);
+            }
+            catch (IOException e)
+            {
+                throw new Exception(e.Message);
+            }
+            return allExamApplications;
+        }
+    }
+}
