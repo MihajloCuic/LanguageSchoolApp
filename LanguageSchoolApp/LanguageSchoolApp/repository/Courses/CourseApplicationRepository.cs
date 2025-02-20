a48d40a5baefddf06cc380c30ab288ac33f1e549
MihajloCuic <158320399+MihajloCuic@users.noreply.github.com>
2025-02-20 13:55:38 +0100

    Added course related repositories

---
+++ LanguageSchoolApp/LanguageSchoolApp/repository/Courses/CourseApplicationRepository.cs
@@ -0,0 +1,44 @@
+﻿using LanguageSchoolApp.model.Courses;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguageSchoolApp.repository.Courses
+{
+    public class CourseApplicationRepository
+    {
+        public static readonly string filename = Path.Combine("..", "..", "..", "data", "CourseApplications.json");
+
+        public static void WriteToFile(Dictionary<int, CourseApplication> allCourseApplications)
+        {
+            try
+            {
+                string serializedCourseApplications = JsonConvert.SerializeObject(allCourseApplications, Formatting.Indented);
+                File.WriteAllText(filename, serializedCourseApplications);
+            }
+            catch (IOException e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        public static Dictionary<int, CourseApplication> ReadFromFile()
+        {
+            Dictionary<int, CourseApplication> allCourseApplications = new Dictionary<int, CourseApplication>();
+            try
+            {
+                string data = File.ReadAllText(filename);
+                allCourseApplications = JsonConvert.DeserializeObject<Dictionary<int, CourseApplication>>(data);
+            }
+            catch (IOException e)
+            {
+                throw new Exception(e.Message);
+            }
+            return allCourseApplications;
+        }
+    }
+}
---
+++ LanguageSchoolApp/LanguageSchoolApp/repository/Courses/CourseRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+using LanguageSchoolApp.model.Courses;
+using System.IO;
+
+namespace LanguageSchoolApp.repository.Courses
+{
+    public class CourseRepository
+    {
+        public static readonly string filename = Path.Combine("..", "..", "..", "data", "Courses.json");
+
+        public static void WriteToFile(Dictionary<int, Course> allCourses) {
+            try {
+                string serializedCourses = JsonConvert.SerializeObject(allCourses, Formatting.Indented);
+                File.WriteAllText(filename, serializedCourses);
+            } catch (IOException e) { 
+                throw new Exception(e.Message);
+            }
+        }
+
+        public static Dictionary<int, Course> ReadFromFile() { 
+            Dictionary<int, Course> allCourses = new Dictionary<int, Course>();
+            try {
+                string data = File.ReadAllText(filename);
+                allCourses = JsonConvert.DeserializeObject<Dictionary<int, Course>>(data);
+            }
+            catch (IOException e) {
+                throw new Exception(e.Message);
+            }
+            return allCourses;
+        }
+    }
+}
