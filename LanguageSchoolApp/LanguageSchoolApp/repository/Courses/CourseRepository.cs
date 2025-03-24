f4ea3b2e865e9514f05ce73712b3b71c94689ee7
MihajloCuic <158320399+MihajloCuic@users.noreply.github.com>
2025-03-24 13:14:04 +0100

    Added CRUD operations logic for Course

--- LanguageSchoolApp/LanguageSchoolApp/repository/Courses/CourseRepository.cs
+++ LanguageSchoolApp/LanguageSchoolApp/repository/Courses/CourseRepository.cs
@@ -7,14 +7,70 @@ using Newtonsoft.Json;
 
 using LanguageSchoolApp.model.Courses;
 using System.IO;
+using LanguageSchoolApp.exceptions.Courses;
+using LanguageSchoolApp.model;
 
 namespace LanguageSchoolApp.repository.Courses
 {
-    public class CourseRepository
+    public class CourseRepository : ICourseRepository
     {
-        public static readonly string filename = Path.Combine("..", "..", "..", "data", "Courses.json");
+        private static readonly string filename = Path.Combine("..", "..", "..", "data", "Courses.json");
+        private readonly Dictionary<int, Course> allCourses;
 
-        public static void WriteToFile(Dictionary<int, Course> allCourses) {
+        public CourseRepository() 
+        {
+            allCourses = ReadFromFile();
+        }
+
+        public Dictionary<int, Course> GetAllCourses() 
+        { 
+            return allCourses;
+        }
+
+        public Course GetCourse(int courseId) 
+        {
+            if (!allCourses.ContainsKey(courseId)) 
+            { 
+                throw new CourseException("Course not found!", CourseExceptionType.CourseNotFound);
+            }
+            return allCourses[courseId];
+        }
+
+        public List<Course> CheckIfCoursesMatch(DateTime beginningDate, CourseType courseType, List<ClassPeriod> classPeriods) 
+        { 
+            List<Course> matchingCourses = new List<Course>();
+            foreach (Course course in allCourses.Values) 
+            {
+                if (course.BeginningDate.Equals(beginningDate) && 
+                    course.CourseType.Equals(courseType) &&
+                    course.ClassPeriods.SequenceEqual(classPeriods)) 
+                { 
+                    matchingCourses.Add(course);
+                }
+            }
+            return matchingCourses;
+        }
+
+        public void AddCourse(Course course) 
+        { 
+            allCourses.Add(course.Id, course);
+            WriteToFile();
+        }
+
+        public void UpdateCourse(int courseId, Course course) 
+        {
+            allCourses[courseId] = course;
+            WriteToFile();
+        }
+
+        public void DeleteCourse(int courseId) 
+        { 
+            allCourses.Remove(courseId);
+            WriteToFile();
+        }
+
+        public void WriteToFile() 
+        {
             try {
                 string serializedCourses = JsonConvert.SerializeObject(allCourses, Formatting.Indented);
                 File.WriteAllText(filename, serializedCourses);
@@ -24,15 +80,15 @@ namespace LanguageSchoolApp.repository.Courses
         }
 
         public static Dictionary<int, Course> ReadFromFile() { 
-            Dictionary<int, Course> allCourses = new Dictionary<int, Course>();
+            Dictionary<int, Course> courses = new Dictionary<int, Course>();
             try {
                 string data = File.ReadAllText(filename);
-                allCourses = JsonConvert.DeserializeObject<Dictionary<int, Course>>(data);
+                courses = JsonConvert.DeserializeObject<Dictionary<int, Course>>(data);
             }
             catch (IOException e) {
                 throw new Exception(e.Message);
             }
-            return allCourses;
+            return courses;
         }
     }
 }
---
+++ LanguageSchoolApp/LanguageSchoolApp/service/Courses/ICourseService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LanguageSchoolApp.model;
+using LanguageSchoolApp.model.Courses;
+
+namespace LanguageSchoolApp.service.Courses
+{
+    public interface ICourseService
+    {
+        Dictionary<int, Course> GetAllCourses();
+        Course GetCourse(int courseId);
+        void CreateCourse(string languageName, string languageLevel, int maxParticipants, int duration, List<KeyValuePair<string, string>> classPeriods, string beginningDate, string courseType, string teacherId);
+        void UpdateCourse(int courseId, string languageName, string languageLevel, int maxParticipants, int duration, List<KeyValuePair<string, string>> classPeriods, string beginningDate, string courseType);
+        void DeleteCourse(int courseId);
+        int GenerateId(LanguageProficiency languageProficiency, DateTime beginningDate, CourseType courseType, string teacherId);
+    }
+}
