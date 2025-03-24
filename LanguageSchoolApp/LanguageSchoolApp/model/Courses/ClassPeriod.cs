d623a1697ebea17a6f31d183b54d257c3f66e7b9
MihajloCuic <158320399+MihajloCuic@users.noreply.github.com>
2025-03-24 13:12:34 +0100

    Added ClassPeriods to Course model

---
+++ LanguageSchoolApp/LanguageSchoolApp/model/Courses/ClassPeriod.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguageSchoolApp.model.Courses
+{
+    public class ClassPeriod
+    {
+        public DaysOfWeek DayOfWeek { get; set; }
+        public TimeOnly StartTime { get; set; }
+
+        public ClassPeriod(DaysOfWeek _dayOfWeek, TimeOnly _startTime) 
+        { 
+            DayOfWeek = _dayOfWeek;
+            StartTime = _startTime;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is ClassPeriod other)
+            { 
+                return this.DayOfWeek == other.DayOfWeek && this.StartTime == other.StartTime;
+            }
+            return false;
+        }
+    }
+}
--- LanguageSchoolApp/LanguageSchoolApp/model/Courses/Course.cs
+++ LanguageSchoolApp/LanguageSchoolApp/model/Courses/Course.cs
@@ -13,40 +13,22 @@ namespace LanguageSchoolApp.model.Courses
         public LanguageProficiency LanguageProficiency { get; set; }
         public int MaxParticipants { get; set; }
         public int Duration { get; set; }
-        public DateTime ClassDays { get; set; }
+        public List<ClassPeriod> ClassPeriods { get; set; }
         public DateTime BeginningDate { get; set; }
         public CourseType CourseType { get; set; }
 
         public Course() { }
 
         public Course(int _id, LanguageProficiency _languageProficiency, int _maxParticipants, 
-            int _duration, DateTime _classDays, DateTime _beginningDate, CourseType _courseType)
+            int _duration, List<ClassPeriod> _classPeriods, DateTime _beginningDate, CourseType _courseType)
         {
             Id = _id;
             LanguageProficiency = _languageProficiency;
             MaxParticipants = _maxParticipants;
             Duration = _duration;
-            ClassDays = _classDays;
+            ClassPeriods = _classPeriods;
             BeginningDate = _beginningDate;
             CourseType = _courseType;
-        }
-
-        public Course(LanguageProficiency _languageProficiency, int _maxParticipants, int _duration, 
-            DateTime _classDays, DateTime _beginningDate, CourseType _courseType) {
-            Id = GenerateId(_languageProficiency, _beginningDate, _courseType);
-            LanguageProficiency = _languageProficiency;
-            MaxParticipants = _maxParticipants;
-            Duration = _duration;
-            ClassDays = _classDays;
-            BeginningDate = _beginningDate;
-            CourseType = _courseType;
-        }
-
-        private int GenerateId(LanguageProficiency languageProficiency, DateTime beginningDate, CourseType courseType) {
-            string combination = languageProficiency.LanguageName + 
-                                languageProficiency.LanguageLevel.ToString() + 
-                                beginningDate.ToString("ddMMyyyy") + courseType.ToString();
-            return combination.GetHashCode();
         }
     }
 }
