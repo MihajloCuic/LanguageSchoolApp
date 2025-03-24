4836c0a2770c40f7bda8f9301f1e3321ef378abb
MihajloCuic <158320399+MihajloCuic@users.noreply.github.com>
2025-03-24 10:38:28 +0100

    Added CRUD operations logic for Director

---
+++ LanguageSchoolApp/LanguageSchoolApp/service/Users/Directors/DirectorService.cs
@@ -0,0 +1,43 @@
+﻿using LanguageSchoolApp.repository.Users.Directors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LanguageSchoolApp.model.Users;
+using LanguageSchoolApp.model;
+using LanguageSchoolApp.service.Users.Validation;
+
+namespace LanguageSchoolApp.service.Users.Directors
+{
+    public class DirectorService : IDirectorService
+    {
+        private readonly IDirectorRepository directorRepository;
+
+        public DirectorService(IDirectorRepository repository)
+        {
+            directorRepository = repository;
+        }
+
+        public Director GetDirector() 
+        { 
+            return directorRepository.GetDirector();
+        }
+
+        public void UpdateDirector(string name, string surname, string genderStr, string birthdayStr, string phoneNumber, string email, string password, string confirmPassword) 
+        {
+            ValidateDirector(name, surname, genderStr, birthdayStr, phoneNumber, email, password, confirmPassword);
+            Gender gender = Enum.Parse<Gender>(genderStr);
+            DateTime birthday = DateTime.Parse(birthdayStr);
+
+            directorRepository.UpdateDirector(name, surname, gender, birthday, phoneNumber, email, password);
+        }
+
+        public bool ValidateDirector(string name, string surname, string genderStr, string birthdayStr, string phoneNumber, string email, string password, string confirmPassword) 
+        {
+            UserValidations.Validate(name, surname, genderStr, birthdayStr, phoneNumber, password, confirmPassword);
+            UserValidations.ValidateEmail(email);
+            return true;
+        }
+    }
+}
---
+++ LanguageSchoolApp/LanguageSchoolApp/service/Users/Directors/IDirectorService.cs
@@ -0,0 +1,17 @@
+﻿using LanguageSchoolApp.model;
+using LanguageSchoolApp.model.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguageSchoolApp.service.Users.Directors
+{
+    public interface IDirectorService
+    {
+        Director GetDirector();
+        void UpdateDirector(string name, string surname, string genderStr, string birthdayStr, string phoneNumber, string email, string password, string confirmPassword);
+        bool ValidateDirector(string name, string surname, string genderStr, string birthdayStr, string phoneNumber, string email, string password, string confirmPassword);
+    }
+}
