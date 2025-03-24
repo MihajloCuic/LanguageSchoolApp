b0a67519f5036a9280d7e3d40149eef6cfd04b5e
MihajloCuic <158320399+MihajloCuic@users.noreply.github.com>
2025-03-24 10:31:27 +0100

    Added Exception types for users

---
+++ LanguageSchoolApp/LanguageSchoolApp/exceptions/Users/UserException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguageSchoolApp.exceptions.Users
+{
+    public enum UserExceptionType 
+    {
+        UserNotFound
+    }
+    public class UserException : Exception
+    {
+        public string Text { get; set; }
+        public UserExceptionType Type { get; set; }
+
+        public UserException(string _text, UserExceptionType _type) 
+        { 
+            Text = _text;
+            Type = _type;
+        }
+    }
+}
